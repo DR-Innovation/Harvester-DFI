d9ada59ba74ccd4655bf3abaf17009b7483610ad
Kræn Hansen <post@creen.dk>
2012-06-25 12:01:00 +0200

    Added the file extractor abstraction.

---
+++ DFIImageExtractor.php
@@ -0,0 +1,85 @@
+<?php
+class DFIImageExtractor extends CHAOSFileExtractor {
+	const DFI_IMAGE_SCANPIX_BASE_PATH = 'http://www2.scanpix.eu/';
+	
+	public $_CHAOSImageFormatID;
+	public $_CHAOSLowResImageFormatID;
+	public $_CHAOSThumbnailImageFormatID;
+	public $_CHAOSImageDestinationID;
+	
+	public static $singleton;
+	/**
+	 * Process the DFI movieitem.
+	 * @param CHAOS\Portal\Client\PortalClient $chaosClient The CHAOS client to use for the importing.
+	 * @param dfi\DFIClient $dfiClient The DFI client to use for importing.
+	 * @param dfi\model\Item $movieItem The DFI movie item.
+	 * @param stdClass $object Representing the DKA program in the CHAOS service, of which the images should be added to.
+	 * @return array An array of processed files.
+	 */
+	function process($chaosClient, $dfiClient, $movieItem, $object) {
+		$imagesProcessed = array();
+		$urlBase = self::DFI_IMAGE_SCANPIX_BASE_PATH;
+		
+		printf("\tUpdating the file for the main (thumbnail) image: ");
+		// Update the thumbnail.
+		$mainImage = $movieItem->xpath("/dfi:MovieItem/dfi:MainImage/dfi:SrcMini");
+		$filenameMatches = array();
+		if(count($mainImage) > 0 && preg_match("#$urlBase(.*)#", $mainImage[0], $filenameMatches) === 1) {
+			$pathinfo = pathinfo($filenameMatches[1]);
+			$response = $this->getOrCreateFile($chaosClient, $object, null, $this->_CHAOSThumbnailImageFormatID, $this->_CHAOSImageDestinationID, $pathinfo['basename'], $pathinfo['basename'], $pathinfo['dirname']);
+			
+			if($response == null) {
+				throw new RuntimeException("Failed to create the main image file.");
+			} else {
+				$object->ProcessedFiles[] = $response;
+				$imagesProcessed[] = $response;
+			}
+		}
+		printf(" Done.\n");
+		
+		
+		$imagesRef = strval($movieItem->Images);
+		if ($imagesRef == null || $imagesRef === '') {
+			printf("\tFound no reference to images:\tDone\n");
+			return;
+		}
+		$images = $dfiClient->load($imagesRef);
+		
+		printf("\tUpdating files for %u images:\t", count($images->PictureItem));
+		//$this->resetProgress(count($images->PictureItem));
+		//$progress = 0;
+		echo self::PROGRESS_END_CHAR;
+		
+		foreach($images->PictureItem as $i) {
+			//$this->updateProgress($progress++);
+			// The following line is needed as they forget to set their encoding.
+			//$i->Caption = iconv( "UTF-8", "ISO-8859-1//TRANSLIT", $i->Caption );
+			//echo "\$caption = $caption\n";
+			//printf("\tFound an image with the caption '%s'.\n", $i->Caption);
+			$imageURLS = array(
+					(string)$i->SrcMini => (integer)$this->_CHAOSImageFormatID,
+					(string)$i->SrcThumb => (integer)$this->_CHAOSLowResImageFormatID);
+				
+			foreach($imageURLS as $url => $formatId) {
+				$filenameMatches = array();
+				if(preg_match("#$urlBase(.*)#", $url, $filenameMatches) === 1) {
+					$pathinfo = pathinfo($filenameMatches[1]);
+					$response = $this->getOrCreateFile($chaosClient, $object, null, $formatId, $this->_CHAOSImageDestinationID, $pathinfo['basename'], $pathinfo['basename'], $pathinfo['dirname']);
+						
+					if($response == null) {
+						throw new RuntimeException("Failed to create an image file.");
+					} else {
+						$object->ProcessedFiles[] = $response;
+						$imagesProcessed[] = $response;
+					}
+				} else {
+					printf("\tWarning: Found an images which was didn't have a scanpix/mini URL. This was not imported.\n");
+				}
+			}
+		}
+		echo self::PROGRESS_END_CHAR;
+		
+		printf(" Done\n");
+		return $imagesProcessed;
+	}
+}
---
+++ DFIVideoExtractor.php
@@ -0,0 +1,49 @@
+<?php
+class DFIVideoExtractor extends CHAOSFileExtractor {
+	const DFI_VIDEO_BASE = 'http://video.dfi.dk/';
+	
+	public $_CHAOSVideoFormatID;
+	public $CHAOSVideoDestinationID;
+	
+	public static $singleton;
+	/**
+	 * Process the DFI movieitem.
+	 * @param CHAOS\Portal\Client\PortalClient $chaosClient The CHAOS client to use for the importing.
+	 * @param dfi\DFIClient $dfiClient The DFI client to use for importing.
+	 * @param dfi\model\Item $movieItem The DFI movie item.
+	 * @param stdClass $object Representing the DKA program in the CHAOS service, of which the images should be added to.
+	 * @return array An array of processed files.
+	 */
+	function process($chaosClient, $dfiClient, $movieItem, $object) {
+		$videosProcessed = array();
+		$urlBase = self::DFI_VIDEO_BASE;
+		
+		$movies = $movieItem->xpath("/dfi:MovieItem/dfi:FlashMovies/dfi:FlashMovieItem");
+		
+		printf("\tUpdating files for %u videos:\t", count($movies));
+		
+		echo self::PROGRESS_END_CHAR;
+		foreach($movies as $m) {
+			// The following line is needed as they forget to set their encoding.
+			//$i->Caption = iconv( "UTF-8", "ISO-8859-1//TRANSLIT", $i->Caption );
+			
+			$miniFilenameMatches = array();
+			if(preg_match("#$urlBase(.*)#", $m->FilmUrl, $miniFilenameMatches) === 1) {
+				$pathinfo = pathinfo($miniFilenameMatches[1]);
+				$response = $this->getOrCreateFile($chaosClient, $object, null, $this->_CHAOSVideoFormatID, $this->_CHAOSVideoDestinationID, $pathinfo['basename'], $pathinfo['basename'], $pathinfo['dirname']);
+				if($response == null) {
+					throw new RuntimeException("Failed to create a video file.");
+				} else {
+					$object->ProcessedFiles[] = $response;
+					$videosProcessed[] = $response;
+				}
+			} else {
+				printf("\tWarning: Found an images which was didn't have a scanpix/mini URL. This was not imported.\n");
+			}
+		}
+		echo self::PROGRESS_END_CHAR;
+		printf(" Done\n");
+		
+		return $videosProcessed;
+	}
+}
